ca74ea2f2777ee7f09ef487b2e5b9c973a7d4e82
JBarra15 <joelbarrasokatu@hotmail.com>
2025-06-04 10:10:36 -0300

    'Mudando-footer'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-blue-900 text-white py-3 mt-8 text-center text-sm">
+    <footer className="bg-blue-700 text-white py-3 text-center text-sm mt-12 shadow-inner">
       © {new Date().getFullYear()} - Projeto GS | Todos os direitos reservados.
     </footer>
   );
